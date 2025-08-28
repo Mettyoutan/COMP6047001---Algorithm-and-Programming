f347c541fd43e690f00c746d66aec148c23b7895
Mettyoutan <Metiu.tan@gmail.com>
2025-08-28 14:53:55 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        final String NAME = "Mathew Tan";
+        final String NIM = "2902555682";
+        System.out.println("Nama saya adalah " + NAME);
+        System.out.println("NIM saya adalah " + NIM);
+
+        System.out.println(
+                "Bahasa pemrograman favorit saya adalah javascript, java, dan sql, karena saya belajar bahasa tersebut"
+        );
+    }
+}
